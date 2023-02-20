ca627a62c807bff3c2619fe40bd47f2338ec74c1
EZIKETOCHI <isrealtochi@gmail.com>
2023-02-19 21:20:46 -0800

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,22 +12,21 @@
 int main(void)
 {
 	int n;
-	int num;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if (num > 5)
+	if (n > 5)
 	{
-		printf("%d and is greater than 5\n", num);
+		printf("%d and is greater than 5\n", n);
 	}
-	else if ((num < 6) && (num < 0))
+	else if ((n < 6) && (n < 0))
 	{
-		printf("%d and is less than 6 and not 0\n", num);
+		printf("%d and is less than 6 and not 0\n", n);
 	}
 	else 
 	{
-		printf("%d and is 0\n", num);
+		printf("%d and is 0\n", n);
 	}
 	return (0);
 }
