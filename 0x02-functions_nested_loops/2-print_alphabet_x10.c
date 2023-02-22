f7e595bdf6429b1a4dfe1e95bf162b1c2b546ca2
EZIKETOCHI <isrealtochi@gmail.com>
2023-02-22 07:06:00 -0800

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,14 +6,15 @@
  */
 
 void print_alphabet_x10(void)
+
 {
 	int i;
 	char j;
-	
-	for (i = 1 ; i <= 10 ; i++)
-	{
 
-	     for (j = 'a' ; j <= 'z' ; j++)
+	for (i = 1 ; i <= 10 ; i++)
+
+	{
+		for (j = 'a' ; j <= 'z' ; j++)
 		_putchar(j);
 	_putchar('\n');
 	}
