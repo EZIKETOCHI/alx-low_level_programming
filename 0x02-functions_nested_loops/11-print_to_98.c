c3b1c8ba95ba50af879eb821860431ac13fe535b
EZIKETOCHI <isrealtochi@gmail.com>
2023-02-22 21:11:48 -0800

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,7 +16,7 @@ if (n >= 0 && n <= 98)
 	{
 for (i = n ; i <= 98 ; i++)
 		{
-printf("%d" , i);
+printf("%d " , i);
 		}
 printf("\n");
 	}
@@ -24,7 +24,7 @@ else if (n < 0)
 {
 for (i = n ; i <= 98 ; i++)
 {
-printf("%d" , i);
+printf("%d " , i);
 }
 printf("\n");
 }
@@ -32,7 +32,7 @@ else if (n > 98)
 {
 for (i = n ; i >= 98 ; i--)
 	{
-printf("%d" , i);
+printf("%d " , i);
 	}
 printf("\n");
 }
