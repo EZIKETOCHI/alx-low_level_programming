38efb1df08e86e20a66248ccc5c3cef2f59ff9ad
EZIKETOCHI <isrealtochi@gmail.com>
2023-02-23 04:13:46 -0800

    task o nested loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - prints upper case letters
+ *
+ * @c: parameter to be checked
+ *
+ * Return: return (0) or (1)
+ */
+int _isupper(int c)
+
+{
+if (c >= 65 && c <= 90)
+return (1);
+else
+return (0);
+
+}
+
