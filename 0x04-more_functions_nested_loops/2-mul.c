fe765a7e4a0e74b9ec70f4cb05fa15df58a2aaee
EZIKETOCHI <isrealtochi@gmail.com>
2023-02-23 04:49:19 -0800

    task 2 nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two digits
+ * @a: parameter to be checked
+ * @b: parameter to be checked
+ * Return: multiplication of int a and int b
+ */
+int mul(int a, int b)
+{
+return (a * b);
+putchar('\n');
+}
