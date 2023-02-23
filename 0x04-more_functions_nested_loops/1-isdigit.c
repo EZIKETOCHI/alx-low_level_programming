0e6d53543cb68b66353fe2030236579a1f7be7af
EZIKETOCHI <isrealtochi@gmail.com>
2023-02-23 04:28:16 -0800

    task 1 nested loops

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isdigit - prints digit from zero to nine
+ *
+ * @c: parameter to be checked
+ *
+ * Return: returns (1) and (0)
+ */
+int _isdigit(int c)
+
+{
+if (c >= 0 && c <= 9)
+return (1);
+else
+return (0);
+
+}
