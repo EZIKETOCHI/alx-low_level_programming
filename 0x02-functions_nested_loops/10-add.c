75007c5183434ea0b86113d1a4e9da5f52b68f0f
EZIKETOCHI <isrealtochi@gmail.com>
2023-02-22 20:58:22 -0800

    task 11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * add: adds two numbers together
+ *
+ * @a: parameter one
+ *
+ * @b: parameter two
+ *
+ * Return: return sum
+ */
+
+int add(int a, int b)
+{
+sum = a + b;
+_putchar('\n');
+return (a + b);
+
+}
