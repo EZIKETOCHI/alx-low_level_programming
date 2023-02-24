255c838c14b31a9a5c9b900eeec4b45af3c020ba
EZIKETOCHI <isrealtochi@gmail.com>
2023-02-23 20:19:26 -0800

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers from (0) to (9)
+ * @i: parameter to be checked
+ * Return: Always (0)
+ */
+void print_numbers(void)
+{
+int i;
+for (i >= 0 ; i <= 9 ; i++)
+putchar("%d ", i);
+putchar('\n');
+return (0);
+}
