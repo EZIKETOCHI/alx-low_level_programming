3b481b742c74317d491e60928805882b331857b3
EZIKETOCHI <isrealtochi@gmail.com>
2023-02-23 20:44:01 -0800

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _most_numbers - prints digit except (2) and (4)
+ *
+ * @i: parameter to be checked 
+ *
+ * Return: Always (0)
+ */
+void print_most_numbers(void)
+{
+int i
+for (i = 0 ; i <= 9 ; i++)
+{
+if (i != 2) && (i != 4)
+{
+putchar;
+}
+putchar ('\n');
+return (0);
+}
