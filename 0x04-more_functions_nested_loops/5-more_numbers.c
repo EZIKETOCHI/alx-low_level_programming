7005ee186d4ebc896c13362e41a859d6ec5a95c3
EZIKETOCHI <isrealtochi@gmail.com>
2023-03-05 13:25:04 -0800

    task 5 more functions nested loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,21 +1,22 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * more_numbers - Prints all single digit numbers of base 10 starting from 0,
- *        only using putchar and without char variables.
- *
- * @num: parameter to be checked
- *
- * Return: Always 0.
+ * more_numbers - print more numbers
  */
+
 void more_numbers(void)
 {
-int num;
+int i, j;
 
-for (num = 0; num < 10; num++)
-putchar((num % 10) + '0');
-
-putchar('\n');
-
-return (0);
+for (i = 1; i <= 10; i++)
+{
+for (j = 0; j <= 14; j++)
+{
+if (j >= 10)
+putchar('1');
+putchar (j % 10 + '0');
 }
+putchar('\n');
+}
+}
+
